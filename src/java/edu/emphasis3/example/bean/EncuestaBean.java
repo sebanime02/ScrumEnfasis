1ce651f52f7a06145e683c31c4178abc8ae5dd50
Javier Hurtado <javier.hurtado@somos.biz>
2015-03-12 11:06:36 -0500

    Primer ejemplo completo de JSF

---
+++ src/java/edu/emphasis3/example/bean/EncuestaBean.java
@@ -0,0 +1,38 @@
+package edu.emphasis3.example.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author javhur
+ */
+@ManagedBean
+@SessionScoped
+public class EncuestaBean {
+    private int response1;
+    private String response2;
+
+    public EncuestaBean() {
+    }
+
+    public int getResponse1() {
+        return response1;
+    }
+
+    public void setResponse1(int response1) {
+        this.response1 = response1;
+    }
+
+    public String getResponse2() {
+        return response2;
+    }
+
+    public void setResponse2(String response2) {
+        this.response2 = response2;
+    }
+
+    public String registrarDatos() {
+        return "result";
+    }
+}
